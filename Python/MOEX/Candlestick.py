0c324959482d26abf30b454ddac1ff5dbb2dc79b
Yury Filyanin <yury.filyanin@yandex.com>
2024-10-08 13:40:47 +0700

    getMarketData added

--- Python/MOEX/Candlestick.py
+++ Python/MOEX/Candlestick.py
@@ -2,8 +2,19 @@ import urllib.request
 import json
 import sqlite3
 
-def getCandles(ticker, date, interval):
-    url = f'https://iss.moex.com/iss/engines/stock/markets/shares/boards/tqbr/securities/{ticker}/candles.json?from={date}&till={date}&interval={interval}'
+def getMarketData(ticker):
+    url = f'https://iss.moex.com/iss/securities/{ticker}.json'
+    response = urllib.request.urlopen(url)
+    data = json.loads(response.read().decode('utf-8'))
+    engineIdx = data['boards']['columns'].index('engine')
+    marketIdx = data['boards']['columns'].index('market')
+    boardIdx = data['boards']['columns'].index('boardid')
+    print('Idx: ', engineIdx, marketIdx, boardIdx)
+    return data['boards']['data'][0][engineIdx], data['boards']['data'][0][marketIdx], data['boards']['data'][0][boardIdx]
+
+def getCandles(ticker, date, interval=24):
+    engine, market, board = getMarketData(ticker)
+    url = f'https://iss.moex.com/iss/engines/{engine}/markets/{market}/boards/{board}/securities/{ticker}/candles.json?from={date}&till={date}&interval={interval}'
     response = urllib.request.urlopen(url)
     data = json.loads(response.read().decode('utf-8'))
     candles = [[ticker, date, interval] + i[:4] for i in data['candles']['data']]
@@ -11,14 +22,13 @@ def getCandles(ticker, date, interval):
 
 def saveToDb(candles, db):
     cursor = db.cursor()
-    cursor.execute('''CREATE TABLE IF NOT EXISTS candles(ticker TEXT, date TEXT, frame INT, open REAL, close REAL, high REAL, low REAL, UNIQUE(ticker, date, frame))''')
-    cursor.executemany('INSERT OR IGNORE INTO candles (ticker, date, frame, open, close, high, low) VALUES (?, ?, ?, ?, ?, ?, ?)', candles)
+    cursor.executemany('INSERT OR IGNORE INTO candles (ticker, date, interval, open, close, high, low) VALUES (?, ?, ?, ?, ?, ?, ?)', candles)
     db.commit()
 
 def smartGetCandles(ticker, date, interval, db):
     cursor = db.cursor()
-    cursor.execute('SELECT * FROM candles WHERE ticker = ? AND date = ? AND frame = ?', (ticker, date, interval))
-    result = cursor.fetchall()
+    cursor.execute('SELECT * FROM candles WHERE ticker = ? AND date = ? AND interval = ?', (ticker, date, interval))
+    result = [list(row) for row in cursor.fetchall()]
     if result:
         print('From SQLite:')
         return result
@@ -37,6 +47,10 @@ def smartInput():
         return ['', '', -1]
 
 db = sqlite3.connect('candles.db')
+cursor = db.cursor()
+cursor.execute('''CREATE TABLE IF NOT EXISTS candles(ticker TEXT, date TEXT, interval INT, open REAL, close REAL, high REAL, low REAL, UNIQUE(ticker, date, interval))''')
+db.commit()
+
 ticket, date, interval = smartInput()
 while ticket != '':
     candle = smartGetCandles(ticket, date, interval, db)
